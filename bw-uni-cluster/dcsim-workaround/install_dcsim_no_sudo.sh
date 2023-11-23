1794711a17c54088e65d718e9cf8c9cb7725f355
Valerii Zhyla <orator9x@gmail.com>
2023-11-23 21:32:09 +0100

    Install directorz moved to variable

--- bw-uni-cluster/dcsim-workaround/install_dcsim_no_sudo.sh
+++ bw-uni-cluster/dcsim-workaround/install_dcsim_no_sudo.sh
@@ -5,13 +5,14 @@ module load compiler/gnu/12.1
 git clone https://github.com/HEPCompSim/DCSim.git
 this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
 pushd DCSim
+INSTALL_LOCATION="/home/kit/kastel/jn1292/local"
 
 # pugixml
 git clone https://github.com/zeux/pugixml.git
 mkdir -p pugixml/build
 pushd pugixml/build
 git checkout tags/v1.12.1
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
@@ -21,7 +22,7 @@ git clone https://github.com/nlohmann/json.git
 mkdir -p json/build
 pushd json/build
 git checkout tags/v3.11.2
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
@@ -31,7 +32,7 @@ git clone https://github.com/google/googletest.git
 mkdir -p googletest/build
 pushd googletest/build
 git checkout tags/release-1.12.1
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
@@ -40,7 +41,7 @@ popd
 git clone --depth 1 --branch "v3.34" https://framagit.org/simgrid/simgrid.git
 mkdir -p simgrid/build
 pushd simgrid/build
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
@@ -50,7 +51,7 @@ git clone --depth 1 --branch "v2.2" https://github.com/wrench-project/wrench.git
 mkdir -p wrench/build
 pushd wrench/build
 # git checkout tags/v.2.1
-cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
@@ -62,7 +63,7 @@ pushd build
 # if you got an error with "wrench-dev.h" - add 
 # include_directories(src/ /home/kit/kastel/jn1292/local/include ${SimGrid_INCLUDE_DIR}/include /usr/local/include /opt/local/include /usr/local/include/wrench ${Boost_INCLUDE_DIR})
 # into CMakeLists
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="/home/kit/kastel/jn1292/local" ../
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" ../
 make -j16
 make install
 popd
