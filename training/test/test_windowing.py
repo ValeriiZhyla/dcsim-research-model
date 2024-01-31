c47c398e45839c3c080533199935fab3f4f50d48
Valerii Zhyla <orator9x@gmail.com>
2024-01-31 13:26:58 +0100

    Windowing fixed

---
+++ training/test/test_windowing.py
@@ -0,0 +1,246 @@
+import unittest
+import pandas as pd
+import numpy as np
+
+from training import windowing
+
+
+class TestCreateWindows(unittest.TestCase):
+
+
+    def test_correct_windowing_one_simulation_no_overlap(self):
+        # Test that windows are of correct size and overlap
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 3
+        overlap_size = 0
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        self.assertEqual(len(windows), 2)
+        self.assert_windows_have_expected_structure(df, input_columns, output_columns, overlap_size, window_size, windows)
+
+
+    def test_correct_windowing_one_simulation_no_overlap_small_windows(self):
+        # Test that windows are of correct size and overlap
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 1
+        overlap_size = 0
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        self.assertEqual(len(windows), 6)
+
+        self.assert_windows_have_expected_structure(df, input_columns, output_columns, overlap_size, window_size, windows)
+
+    def assert_windows_have_expected_structure(self, df, input_columns, output_columns, overlap_size, window_size, windows):
+        for i, (input_data, output_data) in enumerate(windows):
+            self.assertEqual(len(input_data), window_size)
+            self.assertEqual(len(output_data), window_size)
+
+            start_index = i * (window_size - overlap_size)
+            for row_index in range(window_size):
+                expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+    def test_correct_windowing_one_simulation_overlap(self):
+        # Test that windows are of correct size and overlap
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 3
+        overlap_size = 2
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        self.assertEqual(len(windows), 4)
+        self.assert_windows_have_expected_structure(df, input_columns, output_columns, overlap_size, window_size, windows)
+
+    def test_correct_windowing_one_simulation_overlap_larger_window(self):
+        # Test that windows are of correct size and overlap
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 5
+        overlap_size = 2
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        self.assertEqual(len(windows), 2)
+        for window_idx, (input_data, output_data) in enumerate(windows):
+            self.assertEqual(len(input_data), window_size)
+            self.assertEqual(len(output_data), window_size)
+
+            start_index = window_idx * (window_size - overlap_size)
+
+            if window_idx == 0:
+                for row_index in range(5):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+            if window_idx == 1:
+                for row_index in range(3):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+                for row_index in range(3, window_size):
+                    self.assertTrue(np.all(np.array(input_data[row_index]) == 0))
+                    self.assertTrue(np.all(np.array(output_data[row_index]) == 0))
+
+    def test_short_sequence_padding(self):
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 100
+        overlap_size = 0
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+        self.assertEqual(len(windows), 1)
+
+        for i, (input_data, output_data) in enumerate(windows):
+            self.assertEqual(len(input_data), window_size)
+            self.assertEqual(len(output_data), window_size)
+
+            start_index = i * (window_size - overlap_size)
+            for row_index in range(6):
+                expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+            for row_index in range(6, window_size):
+                self.assertTrue(np.all(np.array(input_data[row_index]) == 0))
+                self.assertTrue(np.all(np.array(output_data[row_index]) == 0))
+
+
+
+    def test_last_window_padding(self):
+        # Test padding behavior for the last window in a sequence
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42],
+            'input1': [1, 2, 3, 4, 5, 6],
+            'input2': [8, 9, 10, 11, 12, 13],
+            'output1': [15, 16, 17, 18, 19, 20]
+        })
+        window_size = 5
+        overlap_size = 0
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        for i, (input_data, output_data) in enumerate(windows):
+            start_index = i * (window_size - overlap_size)
+
+            if i == 0:
+                for row_index in range(5):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+            if i == 1:
+                for row_index in range(1):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+                for row_index in range(1, window_size):
+                    self.assertTrue(np.all(np.array(input_data[row_index]) == 0))
+                    self.assertTrue(np.all(np.array(output_data[row_index]) == 0))
+
+
+    def test_correct_grouping(self):
+        # Test that windows are of correct size and overlap
+        df = pd.DataFrame({
+            'simulation_id': [42, 42, 42, 42, 42, 42, 69, 69],
+            'input1': [1, 2, 3, 4, 5, 6, 101, 102],
+            'input2': [8, 9, 10, 11, 12, 13, 103, 104],
+            'output1': [15, 16, 17, 18, 19, 20, 105, 106]
+        })
+        window_size = 5
+        overlap_size = 2
+        input_columns = ['simulation_id', 'input1', 'input2']
+        output_columns = ['output1']
+
+        windows = windowing.create_windows(df, window_size, overlap_size, input_columns, output_columns)
+
+        self.assertEqual(len(windows), 3)
+
+        for window_idx, (input_data, output_data) in enumerate(windows):
+            self.assertEqual(len(input_data), window_size)
+            self.assertEqual(len(output_data), window_size)
+
+            start_index = window_idx * (window_size - overlap_size)
+
+            if window_idx == 0:
+                for row_index in range(5):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+            if window_idx == 1:
+                for row_index in range(3):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+                for row_index in range(3, window_size):
+                    self.assertTrue(np.all(np.array(input_data[row_index]) == 0))
+                    self.assertTrue(np.all(np.array(output_data[row_index]) == 0))
+            if window_idx == 2:
+                for row_index in range(2):
+                    expected_row_inputs = df.loc[start_index + row_index, input_columns].values
+                    self.assertTrue(np.array_equal(np.array(input_data[row_index]), expected_row_inputs))
+
+                    expected_row_outputs = df.loc[start_index + row_index, output_columns].values
+                    self.assertTrue(np.array_equal(np.array(output_data[row_index]), expected_row_outputs))
+
+                for row_index in range(2, window_size):
+                    self.assertTrue(np.all(np.array(input_data[row_index]) == 0))
+                    self.assertTrue(np.all(np.array(output_data[row_index]) == 0))
+
+if __name__ == '__main__':
+    unittest.main()
--- training/windowing.py
+++ training/windowing.py
@@ -1,35 +1,30 @@
 import numpy as np
 
 
-def create_windows(df, window_size, overlap_size, input_columns, output_columns):
+def create_windows(df, window_size, overlap_size, input_columns, output_columns, group_by_column='simulation_id'):
     windowed_data = []
 
     # Group by simulation_id
-    grouped = df.groupby('simulation_id', sort=False)
-    print(f"Grouped by [simulation_id]. {grouped.ngroups} groups were found in dataset.")
+    grouped = df.groupby(group_by_column, sort=False)
+    print(f"Grouped by [{group_by_column}]. {grouped.ngroups} groups were found in dataset.")
 
     for name, group in grouped:
         sequence_length = len(group)
-        if sequence_length < window_size:
-            # Pad short sequences with zeros
-            input_data = pad_sequence(group[input_columns].values, window_size)
-            output_data = pad_sequence(group[output_columns].values, window_size)
-            windowed_data.append((input_data, output_data))
-        else:
-            last_start = sequence_length - window_size
-            for start in range(0, last_start + 1, window_size - overlap_size):  # TODO check
-                end = start + window_size
-                window = group.iloc[start:end]
-                input_data = window[input_columns].values
-                output_data = window[output_columns].values
-                windowed_data.append((input_data, output_data))
 
-            # Handle the last part of the sequence with padding if necessary
-            if last_start + window_size < sequence_length:
-                last_window = group.iloc[last_start:]
-                input_data = pad_sequence(last_window[input_columns].values, window_size)
-                output_data = pad_sequence(last_window[output_columns].values, window_size)
-                windowed_data.append((input_data, output_data))
+        window_start_index_delta = window_size - overlap_size
+        next_window_start = 0
+        while next_window_start < sequence_length:
+            start = next_window_start
+            end = start + window_size
+
+            window = group.iloc[start:end]
+            # Pad window with zeros if last sequence part is too short
+            input_data = pad_sequence(window[input_columns].values, window_size)
+            output_data = pad_sequence(window[output_columns].values, window_size)
+            windowed_data.append((input_data, output_data))
+
+            next_window_start = start + window_start_index_delta
+
     return windowed_data
 
 
